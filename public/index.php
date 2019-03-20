8807bce7e43bf655f17c47780a502d99de448580
MarcusAsp <mackan.a.b@hotmail.com>
2019-03-20 15:11:14 +0100

    wtf

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php require_once('../classes/models/Product.php'); ?>
 
 <?php include('../includes/header.php'); ?>
+<<<<<<< HEAD
     
    
 <h1>Index file test</h1>
@@ -10,6 +11,16 @@ if(isset($_SESSION['user'])){
 }else{
     echo "<h5>Inte inloggad</h5>";
 }
+=======
+
+<h1>Index file test</h1>
+    <?php
+    if(isset($_SESSION['user'])){
+        echo "<h5>Välkommen ".$_SESSION['user']."</h5>";
+    }else{
+        echo "<h5>Inte inloggad</h5>";
+    }
+>>>>>>> parent of 644de89... fixed missing login button
 
 $popularProducts = Product::get_most_popular();
 echo("<h5>Mest populära produkterna just nu!</h5>");
