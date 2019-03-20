a833c51710ace04324779718ed9d73a1acb3386f
Viktor Ström <museumhrgiger@hotmail.com>
2019-03-20 14:15:36 +0000

    Fixing

--- public/index.php
+++ public/index.php
@@ -1,15 +1,14 @@
 <?php require_once('../classes/models/Product.php'); ?>
 
 <?php include('../includes/header.php'); ?>
-    
-   
+
 <h1>Index file test</h1>
-<?php
-if(isset($_SESSION['user'])){
-    echo "<h5>Välkommen ".$_SESSION['user']."</h5>";
-}else{
-    echo "<h5>Inte inloggad</h5>";
-}
+    <?php
+    if(isset($_SESSION['user'])){
+        echo "<h5>Välkommen ".$_SESSION['user']."</h5>";
+    }else{
+        echo "<h5>Inte inloggad</h5>";
+    }
 
 $popularProducts = Product::get_most_popular();
 echo("<h5>Mest populära produkterna just nu!</h5>");
